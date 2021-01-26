5a4307023e4ff19afe5d8bc06ba144c2f681ab73
griezma <griezma@outlook.com>
2021-01-26 13:53:22 +0100

    parent pom added

--- api-gateway/src/test/java/griezma/scgateway/apigateway/HelloServiceIT.java
+++ api-gateway/src/test/java/griezma/scgateway/apigateway/HelloServiceIT.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class HelloServiceIntegrationTest {
+public class HelloServiceIT {
     @Autowired
     WebTestClient client;
 
